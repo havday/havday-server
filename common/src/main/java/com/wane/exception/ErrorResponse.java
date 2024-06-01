81d10f3ddd1277f144666c5168361ec5d118d48a
wan2daaa <wan2daaa@gmail.com>
2024-06-02 02:06:05 +0900

    feat: [common] exception이 오면 그에 맞는 errorcode 내려주게 구현

---
+++ common/src/main/java/com/wane/exception/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.wane.exception;
+
+public record ErrorResponse(
+        String code,
+        String message
+) {
+
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
+    }
+}
