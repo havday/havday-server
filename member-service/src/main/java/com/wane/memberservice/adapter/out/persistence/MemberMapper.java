436702a92a3741ef382e50adc1e74617c595592f
wan2daaa <wan2daaa@gmail.com>
2024-06-03 01:04:16 +0900

    fix: [member-service] mapper 에서 modifiable list 로 수정
    toList() : unmodifiable list
    collect(Collectors.toList()) : modifiable list

--- member-service/src/main/java/com/wane/memberservice/adapter/out/persistence/MemberMapper.java
+++ member-service/src/main/java/com/wane/memberservice/adapter/out/persistence/MemberMapper.java
@@ -6,61 +6,63 @@ import com.wane.memberservice.domain.Address;
 import com.wane.memberservice.domain.Member;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
 public class MemberMapper {
 
-	public Member toDomainEntity(MemberJpaEntity jpaEntity) {
-		return Member.create(
-				jpaEntity.getId(),
-				jpaEntity.getName(),
-				jpaEntity.getEmail(),
-				jpaEntity.getPassword(),
-				jpaEntity.getPhoneNumber(),
-				jpaEntity.getPoint(),
-				jpaEntity.getAuthServiceType(),
-				jpaEntity.getMemberRole(),
-				jpaEntity.getAuthId(),
-				jpaEntity.getAddresses().stream().map(this::toDomainEntity).toList()
-		);
-	}
+    public Member toDomainEntity(MemberJpaEntity jpaEntity) {
+        return Member.create(
+                jpaEntity.getId(),
+                jpaEntity.getName(),
+                jpaEntity.getEmail(),
+                jpaEntity.getPassword(),
+                jpaEntity.getPhoneNumber(),
+                jpaEntity.getPoint(),
+                jpaEntity.getAuthServiceType(),
+                jpaEntity.getMemberRole(),
+                jpaEntity.getAuthId(),
+                jpaEntity.getAddresses().stream().map(this::toDomainEntity).collect(Collectors.toList())
+        );
+    }
 
-	public Address toDomainEntity(AddressJpaEntity jpaEntity) {
-		return Address.create(
-				jpaEntity.getId(),
-				jpaEntity.getName(),
-				jpaEntity.getZipCode(),
-				jpaEntity.getRoadName(),
-				jpaEntity.getDetail(),
-				jpaEntity.getPhoneNumber(),
-				jpaEntity.getRecipient(),
-				jpaEntity.isBaseAddress()
-		);
-	}
+    public Address toDomainEntity(AddressJpaEntity jpaEntity) {
+        return Address.create(
+                jpaEntity.getId(),
+                jpaEntity.getName(),
+                jpaEntity.getZipCode(),
+                jpaEntity.getRoadName(),
+                jpaEntity.getDetail(),
+                jpaEntity.getPhoneNumber(),
+                jpaEntity.getRecipient(),
+                jpaEntity.isBaseAddress()
+        );
+    }
 
-	public MemberJpaEntity toJpaEntity(Member member) {
-		return new MemberJpaEntity(
-				member.getId(),
-				member.getName(),
-				member.getEmail(),
-				member.getPassword(),
-				member.getPhoneNumber(),
-				member.getPoint(),
-				member.getAuthServiceType(),
-				member.getRole(),
-				member.getAddresses().stream().map(this::toJpaEntity).toList(),
-				member.getAuthId()
-		);
-	}
+    public MemberJpaEntity toJpaEntity(Member member) {
+        return new MemberJpaEntity(
+                member.getId(),
+                member.getName(),
+                member.getEmail(),
+                member.getPassword(),
+                member.getPhoneNumber(),
+                member.getPoint(),
+                member.getAuthServiceType(),
+                member.getRole(),
+                member.getAddresses().stream().map(this::toJpaEntity).toList(),
+                member.getAuthId()
+        );
+    }
 
-	public AddressJpaEntity toJpaEntity(Address address) {
-		return new AddressJpaEntity(
-				address.getName(),
-				address.getZipCode(),
-				address.getRoadName(),
-				address.getDetail(),
-				address.getPhoneNumber(),
-				address.getRecipient(),
-				address.isBaseAddress()
-		);
-	}
+    public AddressJpaEntity toJpaEntity(Address address) {
+        return new AddressJpaEntity(
+                address.getName(),
+                address.getZipCode(),
+                address.getRoadName(),
+                address.getDetail(),
+                address.getPhoneNumber(),
+                address.getRecipient(),
+                address.isBaseAddress()
+        );
+    }
 }
