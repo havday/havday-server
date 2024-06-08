6b9ef1ffc7432f49707c71133b35f7703f55d9c5
wan2daaa <wan2daaa@gmail.com>
2024-06-08 17:55:56 +0900

    fix: Address VO 객체로 활용하기 위해서 필드에 final 추가

--- delivery-service/src/main/java/com/wane/deliveryservice/domain/Address.java
+++ delivery-service/src/main/java/com/wane/deliveryservice/domain/Address.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 @Getter
 public class Address {
 
-    private String zipCode;
-    private String roadName;
-    private String detail;
-    private String phoneNumber;
-    private String recipient;
+    private final String zipCode;
+    private final String roadName;
+    private final String detail;
+    private final String phoneNumber;
+    private final String recipient;
+
+    public static Address create(String zipCode, String roadName, String detail, String phoneNumber, String recipient) {
+        return new Address(zipCode, roadName, detail, phoneNumber, recipient);
+    }
 
     private Address(String zipCode, String roadName, String detail, String phoneNumber, String recipient) {
         validateAddressRequiredFields(zipCode, roadName, detail, phoneNumber, recipient);
@@ -21,13 +25,14 @@ public class Address {
         this.recipient = recipient;
     }
 
-    private static void validateAddressRequiredFields(String zipCode, String roadName, String detail, String phoneNumber, String recipient) {
-        if (zipCode.isEmpty() || roadName.isEmpty() || detail.isEmpty() || phoneNumber.isEmpty() || recipient.isEmpty()) {
+    private void validateAddressRequiredFields(String zipCode, String roadName, String detail, String phoneNumber, String recipient) {
+        if (zipCode == null || zipCode.isEmpty() ||
+                roadName == null || roadName.isEmpty() ||
+                detail == null || detail.isEmpty() ||
+                phoneNumber == null || phoneNumber.isEmpty() ||
+                recipient == null || recipient.isEmpty()) {
             throw new IllegalArgumentException("주소 정보는 필수 값입니다.");
         }
     }
 
-    public static Address create(String zipCode, String roadName, String detail, String phoneNumber, String recipient) {
-        return new Address(zipCode, roadName, detail, phoneNumber, recipient);
-    }
 }
